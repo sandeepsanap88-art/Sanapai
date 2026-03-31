27b602447bdaa5089e30eac0ee7caa62dd0f3491
sandeepsanap88-art <sandeepsanap88@gmail.com>
2026-04-01 02:51:05 +0530

    Update chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,3 @@
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:google_fonts/google_fonts.dart';

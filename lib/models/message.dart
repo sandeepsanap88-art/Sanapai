b8409113778ceffafe4aa4546349ad6322124e24
sandeepsanap88-art <sandeepsanap88@gmail.com>
2026-04-01 01:07:15 +0530

    Update message.dart

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,1 +1,16 @@
+class Message {
+  final String text;
+  final bool isUser;
+  final DateTime timestamp;
 
+  Message({
+    required this.text,
+    required this.isUser,
+    required this.timestamp,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'role': isUser ? 'user' : 'assistant',
+        'content': text,
+      };
+}

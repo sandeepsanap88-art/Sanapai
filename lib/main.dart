da39a8efb6f6d9d654a829b5bbf0d09d129a391e
sandeepsanap88-art <sandeepsanap88@gmail.com>
2026-04-01 01:32:06 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'screens/chat_screen.dart';
+
+void main() {
+  runApp(const SanapAIApp());
+}
+
+class SanapAIApp extends StatelessWidget {
+  const SanapAIApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'सानप AI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xFF0A0E1A),
+        colorScheme: const ColorScheme.dark(
+          primary: Color(0xFF00D4FF),
+          secondary: Color(0xFF7B2FBE),
+        ),
+      ),
+      home: const ChatScreen(),
+    );
+  }
+}
